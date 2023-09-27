e62f1866a2b803b32785ca42853921b93835656b
iratecat1 <iratecat1@outlook.com>
2023-09-27 17:05:04 +0800

    update

--- src/radar2023/RadarClass/Tracker/include/datatype.h
+++ src/radar2023/RadarClass/Tracker/include/datatype.h
@@ -76,4 +76,43 @@ typedef struct t
 // linear_assignment:
 typedef Eigen::Matrix<float, -1, -1, Eigen::RowMajor> DYNAMICM;
 
-#endif // DEEPSORTDATATYPE_H
+enum DETECTBOX_IDX
+{
+    IDX_X = 0,
+    IDX_Y,
+    IDX_W,
+    IDX_H
+};
+
+class DETECTION_ROW
+{
+private:
+    const float kRatio = 0.5;
+
+public:
+    DETECTBOX tlwh;
+    float confidence;
+    FEATURE feature;
+    DETECTBOX to_xyah() const
+    {
+        //(centerx, centery, ration, h)
+        DETECTBOX ret = tlwh;
+        ret(0, IDX_X) += (ret(0, IDX_W) * kRatio);
+        ret(0, IDX_Y) += (ret(0, IDX_H) * kRatio);
+        ret(0, IDX_W) /= ret(0, IDX_H);
+        return ret;
+    }
+    DETECTBOX to_tlbr() const
+    {
+        //(x,y,xx,yy)
+        DETECTBOX ret = tlwh;
+        ret(0, IDX_X) += ret(0, IDX_W);
+        ret(0, IDX_Y) += ret(0, IDX_H);
+        return ret;
+    }
+};
+
+typedef std::vector<DETECTION_ROW> DETECTIONS;
+typedef std::pair<std::vector<CLSCONF>, DETECTIONS> DETECTIONSV2;
+
+#endif
